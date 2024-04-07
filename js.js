8469b1723d4d1e8181964635f844566229f2d074
Muhammed-Binyamin128 <165582351+binyaminvm@users.noreply.github.com>
2024-04-07 14:44:13 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var dots = document.querySelector("#dots");
+  var moreText = document.querySelector("#more");
+  var btnText = document.querySelector("#myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
